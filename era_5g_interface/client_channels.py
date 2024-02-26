008c1e0f8710e2bee7630e0b9b4f88c08c80b740
Petr Klepárník <ikleparnik@fit.vutbr.cz>
2024-02-26 15:49:41 +0100

    feat: new version with heartbeat and measuring

--- era_5g_interface/client_channels.py
+++ era_5g_interface/client_channels.py
@@ -1,5 +1,6 @@
 import logging
-from typing import Any, Callable, Dict, Optional
+import time
+from typing import Any, Dict
 
 import socketio
 
@@ -20,7 +21,6 @@ class ClientChannels(Channels):
         self,
         sio: socketio.Client,
         callbacks_info: Dict[str, CallbackInfoClient],
-        disconnect_callback: Optional[Callable] = None,
         **kwargs,
     ):
         """Constructor.
@@ -28,15 +28,14 @@ class ClientChannels(Channels):
         Args:
             sio (socketio.Client): Socketio Client object.
             callbacks_info (Dict[str, CallbackInfoClient]): Callbacks Info dictionary, key is custom event name.
-            disconnect_callback (Callable, optional): Triggered on client side before _shutdown on unhandled exception.
+            disconnect_callback (Callable, optional): Triggered before _shutdown on unhandled exception.
             back_pressure_size (int, optional): Back pressure size - max size of eio.queue.qsize().
             recreate_coder_attempts_count (int): How many times try to recreate the video frame encoder/decoder.
             stats (bool): Store output data sizes.
+            extended_measuring (bool): Enable logging of measuring.
         """
 
         super().__init__(sio, callbacks_info, **kwargs)
-
-        self._disconnect_callback = disconnect_callback
 
         self._sio.on(DATA_ERROR_EVENT, lambda data: self.data_error_callback(data), namespace=DATA_NAMESPACE)
 
@@ -71,14 +70,19 @@ class ClientChannels(Channels):
             event (str): Event name.
         """
 
+        timestamp = Channels.get_timestamp_from_data(data)
         cb_info = self._callbacks_info[event]
 
         try:
-            return cb_info.callback(data)
+            self._input_measuring.log_timestamp(timestamp, "before_callback_timestamp")
+            return_value = cb_info.callback(data)
+            self._input_measuring.log_timestamp(timestamp, "after_callback_timestamp")
+            self._input_measuring.log_measuring(timestamp, "eio_sid", self.get_client_eio_sid(None, DATA_NAMESPACE))
+            self._input_measuring.log_measuring(timestamp, "event", event)
+            self._input_measuring.store_measuring(timestamp)
+            return return_value
         except Exception:
-            if self._disconnect_callback:
-                self._disconnect_callback()
-            Channels._shutdown("JSON", event)
+            self._shutdown("JSON", event)
 
     def json_lz4_callback(self, data: bytes, event: str) -> Any:
         """Allows to receive LZ4 compressed general JSON data on DATA_NAMESPACE.
@@ -88,8 +92,13 @@ class ClientChannels(Channels):
             event (str): Event name.
         """
 
+        before_lz4_decode_timestamp = time.perf_counter_ns()
         decoded_data = super().data_lz4_decode(data, event)
         if decoded_data:
+            after_lz4_decode_timestamp = time.perf_counter_ns()
+            timestamp = Channels.get_timestamp_from_data(decoded_data, default=before_lz4_decode_timestamp)
+            self._input_measuring.log_measuring(timestamp, "before_lz4_decode_timestamp", before_lz4_decode_timestamp)
+            self._input_measuring.log_measuring(timestamp, "after_lz4_decode_timestamp", after_lz4_decode_timestamp)
             return self.json_callback(decoded_data, event)
 
     def image_callback(self, data: Dict[str, Any], event: str) -> Any:
@@ -100,13 +109,21 @@ class ClientChannels(Channels):
             event (str): Event name.
         """
 
+        timestamp = Channels.get_timestamp_from_data(data)
         cb_info = self._callbacks_info[event]
 
+        self._input_measuring.log_timestamp(timestamp, "before_decode_timestamp")
         decoded_data = super().image_decode(data, event)
+        self._input_measuring.log_timestamp(timestamp, "after_decode_timestamp")
+
         if decoded_data:
             try:
-                return cb_info.callback(decoded_data)
+                self._input_measuring.log_timestamp(timestamp, "before_callback_timestamp")
+                return_value = cb_info.callback(decoded_data)
+                self._input_measuring.log_timestamp(timestamp, "after_callback_timestamp")
+                self._input_measuring.log_measuring(timestamp, "eio_sid", self.get_client_eio_sid(None, DATA_NAMESPACE))
+                self._input_measuring.log_measuring(timestamp, "event", event)
+                self._input_measuring.store_measuring(timestamp)
+                return return_value
             except Exception:
-                if self._disconnect_callback:
-                    self._disconnect_callback()
-                Channels._shutdown("image", event)
+                self._shutdown("image", event)
--- era_5g_interface/server_channels.py
+++ era_5g_interface/server_channels.py
@@ -1,5 +1,6 @@
 import logging
-from typing import Any, Callable, Dict, Optional
+import time
+from typing import Any, Dict
 
 import socketio
 
@@ -20,7 +21,6 @@ class ServerChannels(Channels):
         self,
         sio: socketio.Server,
         callbacks_info: Dict[str, CallbackInfoServer],
-        disconnect_callback: Optional[Callable] = None,
         **kwargs,
     ):
         """Constructor.
@@ -28,14 +28,14 @@ class ServerChannels(Channels):
         Args:
             sio (socketio.Server): Socketio Server object.
             callbacks_info (Dict[str, CallbackInfoServer]): Callbacks Info dictionary, key is custom event name.
+            disconnect_callback (Callable, optional): Triggered before _shutdown on unhandled exception.
             back_pressure_size (int, optional): Back pressure size - max size of eio.queue.qsize().
             recreate_coder_attempts_count (int): How many times try to recreate the frame encoder/decoder.
             stats (bool): Store output data sizes.
+            extended_measuring (bool): Enable logging of measuring.
         """
 
         super().__init__(sio, callbacks_info, **kwargs)
-
-        self._disconnect_callback = disconnect_callback
 
         self._sio.on(DATA_ERROR_EVENT, lambda sid, data: self.data_error_callback(data, sid), namespace=DATA_NAMESPACE)
 
@@ -71,14 +71,19 @@ class ServerChannels(Channels):
             sid (str, optional): Namespace sid - only on the server side.
         """
 
+        timestamp = Channels.get_timestamp_from_data(data)
         cb_info = self._callbacks_info[event]
 
         try:
-            return cb_info.callback(sid, data)
+            self._input_measuring.log_timestamp(timestamp, "before_callback_timestamp")
+            return_value = cb_info.callback(sid, data)
+            self._input_measuring.log_timestamp(timestamp, "after_callback_timestamp")
+            self._input_measuring.log_measuring(timestamp, "eio_sid", self.get_client_eio_sid(sid, DATA_NAMESPACE))
+            self._input_measuring.log_measuring(timestamp, "event", event)
+            self._input_measuring.store_measuring(timestamp)
+            return return_value
         except Exception:
-            if self._disconnect_callback:
-                self._disconnect_callback(sid, DATA_NAMESPACE)
-            Channels._shutdown("JSON", event)
+            self._shutdown("JSON", event, sid)
 
     def json_lz4_callback(self, data: bytes, event: str, sid: str) -> Any:
         """Allows to receive LZ4 compressed general JSON data on DATA_NAMESPACE.
@@ -89,8 +94,13 @@ class ServerChannels(Channels):
             sid (str, optional): Namespace sid - only on the server side.
         """
 
+        before_lz4_decode_timestamp = time.perf_counter_ns()
         decoded_data = super().data_lz4_decode(data, event, sid)
         if decoded_data:
+            after_lz4_decode_timestamp = time.perf_counter_ns()
+            timestamp = Channels.get_timestamp_from_data(decoded_data, default=before_lz4_decode_timestamp)
+            self._input_measuring.log_measuring(timestamp, "before_lz4_decode_timestamp", before_lz4_decode_timestamp)
+            self._input_measuring.log_measuring(timestamp, "after_lz4_decode_timestamp", after_lz4_decode_timestamp)
             return self.json_callback(decoded_data, event, sid)
 
     def image_callback(self, data: Dict[str, Any], event: str, sid: str) -> Any:
@@ -102,13 +112,21 @@ class ServerChannels(Channels):
             sid (str, optional): Namespace sid - only on the server side.
         """
 
+        timestamp = Channels.get_timestamp_from_data(data)
         cb_info = self._callbacks_info[event]
 
+        self._input_measuring.log_timestamp(timestamp, "before_decode_timestamp")
         decoded_data = super().image_decode(data, event, sid)
+        self._input_measuring.log_timestamp(timestamp, "after_decode_timestamp")
+
         if decoded_data:
             try:
-                return cb_info.callback(sid, decoded_data)
+                self._input_measuring.log_timestamp(timestamp, "before_callback_timestamp")
+                return_value = cb_info.callback(sid, decoded_data)
+                self._input_measuring.log_timestamp(timestamp, "after_callback_timestamp")
+                self._input_measuring.log_measuring(timestamp, "eio_sid", self.get_client_eio_sid(sid, DATA_NAMESPACE))
+                self._input_measuring.log_measuring(timestamp, "event", event)
+                self._input_measuring.store_measuring(timestamp)
+                return return_value
             except Exception:
-                if self._disconnect_callback:
-                    self._disconnect_callback(sid, DATA_NAMESPACE)
-                Channels._shutdown("image", event)
+                self._shutdown("image", event, sid)
